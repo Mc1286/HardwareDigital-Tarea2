b222d07ba0603da3be492b05f478d63d20874c9d
Matías Alejandro Cabrera Bustamante <matias.cabrerab@alumnos.uv.cl>
2026-06-10 20:26:13 -0400

    Esclavo V2: Lectura de LDR y respuesta serial básica
    - Se implementó la lógica de recepción mediante polling directamente en el loop() principal.
    - Se agregó el filtrado básico por byte de sincronización (SOF = 0xAA) y la identificación de la solicitud del Maestro (ID = 0x01).
    - Se incorporó la lectura del sensor analógico (ADC) y la separación del entero de 16 bits en dos fragmentos de 8 bits (highByte y lowByte) para su transmisión por UART.
    - (Pendiente para la V3: Migrar la recepción a la interrupción serialEvent e integrar la validación por Checksum).

--- Codigo_Nodo_Esclavo.ino
+++ Codigo_Nodo_Esclavo.ino
@@ -1,10 +1,23 @@
-// Nodo Esclavo - V1: Estructura base
+// Nodo Esclavo - V2: Respuesta en el loop
 const int pinLDR = A1;
+const byte SOF = 0xAA;
 
 void setup() {
   Serial.begin(9600); 
 }
 
 void loop() {
-  // Pendiente: Recibir peticiones
+  if (Serial.available() >= 2) {
+    if (Serial.read() == SOF) {
+      byte idRecepcionado = Serial.read();
+      
+      if (idRecepcionado == 0x01) {
+        int valorCrudo = analogRead(pinLDR); 
+        Serial.write(SOF);
+        Serial.write(0x02);
+        Serial.write(highByte(valorCrudo));
+        Serial.write(lowByte(valorCrudo));
+      }
+    }
+  }
 }
